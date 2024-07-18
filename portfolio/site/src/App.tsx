c90c110f18eebc61ef765d125de26dd5477ad87c
rajahwu <vmradford@gmail.com>
2024-07-18 03:12:20 -0400

    build: config tailwindcss

--- portfolio/site/src/App.tsx
+++ portfolio/site/src/App.tsx
@@ -1,11 +1,20 @@
-import "./App.css";
+import "./bundle.css";
+import { FlipWords } from "../ui/flip-words";
 
 function App() {
+  const words = ["better", "cute", "beautiful", "modern"];
   return (
     <>
       <div>
         <h1>Portfolio</h1>
         <p>Rajawu</p>
+      </div>
+      <div className="h-[40rem] flex justify-center items-center px-4">
+        <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+          Build
+          <FlipWords words={words} /> <br />
+          websites with Aceternity UI
+        </div>
       </div>
     </>
   );
